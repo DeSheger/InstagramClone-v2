63d32e4bc7acaff42fe397d795c4169e81b9a120
DeSheger <pgsteam@interia.pl>
2021-12-21 15:21:07 +0100

    containers add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import './App.css';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, reset } from './features/counter/counterSlice';
 import {menuHandler} from './features/menu/menuSlice';
 
 function App() {
@@ -10,11 +8,9 @@ function App() {
   const dispatch = useDispatch();
   console.log(menu.getObjectName)
   return (
-    <div className="App">
+    <div className="app">
       <div className='wrapper'>
-        {menu.map(item => console.log(item))}
-
-        <button onClick={() => dispatch(menuHandler({name:'home'}))}>home</button>
+        
       </div>
     </div>
   );
---
+++ src/containers/Favourite.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Favourite extends React.Component {
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
