46453e52977d2ec99017f75096992f1b08ea5901
DeSheger <pgsteam@interia.pl>
2022-01-18 16:48:30 +0100

    Profile add context

--- src/components/profileInfo.js
+++ src/components/profileInfo.js
@@ -1,19 +1,24 @@
-
+import { useSelector } from 'react-redux';
+import user_info from '../data/users_data/users';
 
 export default function ProfileInfo(props) {
+    const userID = useSelector(state => state.session.userID)
+    console.log(userID)
     return (
         <div className="profile__info">
             <div className="profile__info-avatar">
-                <img src={props.avatar} alt="avatar"/>
+                <img src={user_info.user.main[userID].image} alt="avatar"/>
             </div>
-            <div className="profile__info-name">
-                <h1>{props.name}</h1>
-            </div>
-            <div className="profile__info-status">
-                <h2>{props.status}</h2>
-            </div>
-            <div className="profile__info-about">
-                <p>{props.about}</p>
+            <div className="profile__info-desc">
+                <div className="profile__info-name">
+                    <h1>{user_info.user.main[userID].login}</h1>
+                </div>
+                <div className="profile__info-status">
+                    <p>{user_info.user.main[userID].desc}</p>
+                </div>
+                <div className="profile__info-about">
+                    <button>Log Out</button>
+                </div>
             </div>
         </div>
     )
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,13 +1,16 @@
 import Menu from "../components/menu";
 import ProfileInfo from "../components/profileInfo";
 import PostRenderer from "../components/postRenderer";
+import { useState } from "react";
 
 export default function Profile(props) {
+  const [descActive, setDescActive] = useState(false);
+
   return (
     <div className="profile">
       <Menu />
       <ProfileInfo />
-      <PostRenderer />
+      <PostRenderer descActive={descActive} row={true}/>
     </div>
   );
 
